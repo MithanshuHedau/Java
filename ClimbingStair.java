894a9965ae414259650d9d69421c8534059b5f96
mithanshu hedau <hedaumithanshu@gmail.com>
2024-07-19 10:34:59 +0530

    ClimbingStair

---
+++ ClimbingStair.java
@@ -0,0 +1,17 @@
+public class ClimbingStair {
+  public static void stair(int curr, int n, String ans) {
+    if (curr == n) {
+      System.out.println(ans);
+      return;
+    }
+    if (curr > n) {
+      return;
+    }
+    stair(curr + 1, n, ans + 1);
+    stair(curr + 2, n, ans + 2);
+  }
+
+  public static void main(String[] args) {
+    stair(0, 5, " ");
+  }
+}
