ea3335b3a0a366b12c8ace9c8c5b25df86cd0b5e
mithanshu hedau <hedaumithanshu@gmail.com>
2024-07-28 11:57:39 +0530

    Board-Path

---
+++ Stairs.java
@@ -0,0 +1,18 @@
+public class Stairs {
+  public static void main(String[] args) {
+    printans(4, 0, "");
+  }
+
+  public static void printans(int n, int curr, String ans) {
+    if (curr == n) {
+      System.out.println(ans);
+      return;
+    }
+    if (curr > n) {
+      return;
+    }
+    printans(n, curr + 1, ans + 1);
+    printans(n, curr + 2, ans + 2);
+    printans(n, curr + 3, ans + 3);
+  }
+}
