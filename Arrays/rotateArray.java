a6e234e2d59f6693691554a0e616d0436346d141
mithanshu hedau <hedaumithanshu@gmail.com>
2024-08-11 13:36:04 +0530

    RotateArray

--- Arrays/rotateArray.java
+++ Arrays/rotateArray.java
@@ -21,5 +21,26 @@ public class rotateArray {
     for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i]+" ");
     }
+
+
+    /*
+     int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+    int n = 7;
+    int d = 3;
+    int temp[] = new int[d];  
+    d = d % n;
+    for (int i = 0; i < d; i++) {
+      temp[i] = arr[i];
+    }
+    for (int i = d; i < n; i++) {
+      arr[i - d] = arr[i];
+    }
+    for (int i = n - d; i < arr.length; i++) {
+      arr[i] = temp[i - (n - d)];
+    }
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i]+" ");
+    }
+     */
   }
 }
