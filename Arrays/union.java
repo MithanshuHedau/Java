ba3999d1dabe7c7474db97f56022f59c32b44725
mithanshu hedau <hedaumithanshu@gmail.com>
2024-08-12 12:20:38 +0530

    union of two arrays

---
+++ Arrays/union.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.HashSet;
+
+public class union {
+  public static void main(String[] args) {
+    int arr1[] = { 1, 2, 2, 3, 4, 5 };
+    int arr2[] = { 2, 3, 4, 4, 5 };
+    HashSet<Integer> union = new HashSet<>();
+
+    for (int i = 0; i < arr1.length; i++) {
+      union.add(arr1[i]);
+    }
+    for (int i = 0; i < arr2.length; i++) {
+      union.add(arr2[i]);
+    }
+    System.out.println(union);
+  }
+}
