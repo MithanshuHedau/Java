5cb10aa41e21d0d65385bd66ee524615f6d286c0
mithanshu hedau <hedaumithanshu@gmail.com>
2024-09-10 19:47:35 +0530

    0,11,222,3333

---
+++ Patterns/p2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+0
+11
+222
+3333
+*/
+int main()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j <= i; j++)
+    {
+      cout << i;
+    }
+    cout << endl;
+  }
+  return 0;
+}
