d31124c611e9a2cef8b5a894bc626b2cf4f2bd35
mithanshu hedau <hedaumithanshu@gmail.com>
2024-09-10 19:34:46 +0530

    123,456,789

--- Patterns/p1.cpp
+++ Patterns/p1.cpp
@@ -7,17 +7,16 @@ using namespace std;
 */
 int main()
 {
-  int n = 4 ; 
-  int num = 1 ;
+  int n = 4;
+  int num = 1;
   for (int i = 0; i < 3; i++)
   {
     for (int j = 0; j < 3; j++)
     {
-      cout << num;
+      cout << num << " ";
       num++;
     }
     cout << endl;
   }
   return 0;
 }
-
