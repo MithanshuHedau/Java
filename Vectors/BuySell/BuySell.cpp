77709e3dbdb7aa6653e494cec6d96d44e975d3d2
mithanshu hedau <hedaumithanshu@gmail.com>
2024-09-30 20:38:54 +0530

    BuySell

--- Vectors/BuySell/BuySell.cpp
+++ Vectors/BuySell/BuySell.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
-  
+  vector<int> prices = {7, 1, 5, 3, 6, 4};
+  int maxprofit = 0;
+  int bestbuy = prices[0];
+
+  for (int i = 1; i < prices.size(); i++)
+  {
+    if (prices[i] > bestbuy)
+    {
+      maxprofit = max(maxprofit, prices[i] - bestbuy);
+    }
+    bestbuy = min(bestbuy, prices[i]);
+  }
+  cout << maxprofit;
   return 0;
 }
