30936264bbcfeff9b2f50da036afc75beaad571c
mithanshu hedau <hedaumithanshu@gmail.com>
2024-10-01 21:54:47 +0530

    BuySell

--- Vectors/BuySell/BuySell.cpp
+++ Vectors/BuySell/BuySell.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 int main()
 {
-  vector<int> prices = {7, 1, 5, 3, 6, 4};
-  int maxprofit = 0;
-  int bestbuy = prices[0];
+  int arr[] = {7, 1, 5, 3, 6, 4};
+  int n = sizeof(arr) / sizeof(arr[0]);
 
-  for (int i = 1; i < prices.size(); i++)
+  int minNum = arr[0];
+  int profit = 0;
+  int cost;
+
+  for (int i = 1; i < n; i++)
   {
-    if (prices[i] > bestbuy)
-    {
-      maxprofit = max(maxprofit, prices[i] - bestbuy);
-    }
-    bestbuy = min(bestbuy, prices[i]);
+    cost = arr[i] - minNum;
+    profit = max(profit, cost);
+    minNum = min(minNum, arr[i]);
   }
-  cout << maxprofit;
+  cout << profit;
+
   return 0;
 }
