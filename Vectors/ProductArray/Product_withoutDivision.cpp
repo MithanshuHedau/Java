21f1b9f94fc5a63f0f51940f083694b0071f121c
mithanshu hedau <hedaumithanshu@gmail.com>
2024-10-06 19:43:02 +0530

    Without Division

---
+++ Vectors/ProductArray/Product_withoutDivision.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  vector<int> vec1 = {1, 2, 3, 4};
+  vector<int> vec2;
+  int mul;
+
+  for (int i = 0; i < vec1.size(); i++)
+  {
+    mul = 1;
+    for (int j = 0; j < vec1.size(); j++)
+    {
+      if (i == j)
+      {
+        continue;
+      }
+      mul *= vec1[j];
+    }
+    vec2.push_back(mul);
+  }
+  for (int i = 0; i < vec2.size(); i++)
+  {
+    cout << vec2[i] << " ";
+  }
+  return 0;
+}
